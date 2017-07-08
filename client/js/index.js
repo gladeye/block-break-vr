c962dbed0a3e02a19d8904f71d32be5a12620aad
Benjamin Sharp <benjamin.sharp@gladeye.co.nz>
2017-07-08 15:59:40 +1200

    Implement gun component with bomb

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,8 @@
 var AFRAME = require('aframe');
 require('networked-aframe');
+require('networked-aframe/server/static/js/gun.component');
+require('networked-aframe/server/static/js/forward.component');
+require('networked-aframe/server/static/js/remove-in-seconds.component');
 require('networked-aframe/server/static/js/spawn-in-circle.component');
 require('aframe-environment-component/index.js');
 require('aframe-gradient-sky');
@@ -8,4 +11,3 @@ require('aframe-particle-system-component');
 require('./components/intersection-spawn-multi');
 require('./components/snap');
 require('./components/main');
-
