082ea2b89574cd963f386420eefa8d8e7fbc3c57
Michael Andrew <michael@gladeye.co.nz>
2017-07-09 15:02:19 +1200

    Added gun firing sounds

--- client/js/components/gun.component.js
+++ client/js/components/gun.component.js
@@ -19,6 +19,10 @@ AFRAME.registerComponent('gun', {
 
     createBullet: function () {
         var tip = document.querySelector('#player .gun-tip');
+
+        this.playGunSound(tip);
+
+
         var sceneEl = tip.sceneEl;
 
         var entity = document.createElement('a-entity');
@@ -89,24 +93,38 @@ AFRAME.registerComponent('gun', {
 
         //hitEl.setAttribute('sound','
 
+        hitEl.addEventListener('sound-ended',function(){
+            hitEl.parentNode.parentNode.removeChild(hitEl.parentNode);
+        });
+
+        this.playExplodeSound(hitEl);
+
+
+
+        bulletEl.parentNode.removeChild(bulletEl);
+
+        //TODO: spawn explosion at world coordinates then delete the bomb
+        //TODO: add logic to explosion to remove collided entities
+        console.log('Hit voxel');
+    },
+
+    playGunSound: function(tipEl) {
+
+        var soundArray = ['#blip0-sound','#blip1-sound','#blip2-sound'];
+        var randomKey = Math.floor(Math.random() * (soundArray.length - 1 + 1)) + 0;
+
+        tipEl.setAttribute('sound','src',soundArray[randomKey]);
+
+        tipEl.components.sound.playSound();
+    },
+
+    playExplodeSound: function(hitEl) {
         var soundArray = ['#explode0-sound','#explode1-sound','#explode2-sound','#explode3-sound'];
         var randomKey = Math.floor(Math.random() * (soundArray.length - 1 + 1)) + 0;
 
         hitEl.setAttribute('sound','src',soundArray[randomKey]);
 
         hitEl.components.sound.playSound();
-
-        bulletEl.parentNode.removeChild(bulletEl);
-
-        setTimeout(function(){
-            hitEl.parentNode.parentNode.removeChild(hitEl.parentNode);
-        },250);
-
-
-
-        //TODO: spawn explosion at world coordinates then delete the bomb
-        //TODO: add logic to explosion to remove collided entities
-        console.log('Hit voxel');
     }
 
 });
