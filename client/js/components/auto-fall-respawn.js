3248639bbec6d172f65434e4e847fe18c4e90fd7
Michael Andrew <michael.andrew@gladeye.com>
2017-07-24 20:29:50 +1200

    Updated auto-fall-respawn  component
    Allows setting the `respawnPosition`

--- client/js/components/auto-fall-respawn.js
+++ client/js/components/auto-fall-respawn.js
@@ -1,18 +1,54 @@
 AFRAME.registerComponent('auto-fall-respawn', {
     schema: {
-        height: {default: -50}
+        fallHeight: {type: 'int', default: -50},
+        respawnPosition: {type: 'vec3', default: {x: 0, y: 20, z: 0}}
     },
+
+    originalPhysicsType: '',
 
     init: function() {
 
+        var physicsType = this.getPhysicsType();
+
+        if(physicsType !== 'none'){
+            this.originalPhysicsType = this.el.getAttribute(physicsType);
+        }
     },
 
     tick: function() {
 
         var position = this.el.object3D.position;
 
-        if(position.y < this.data.height){
-            this.el.setAttribute('position','0 20 0');
+        //console.log(position.y,this.data.fallHeight);
+
+        if(position.y < this.data.fallHeight){
+
+            var physicsType = this.getPhysicsType();
+
+            if(physicsType === 'dynamic-body'){
+                this.el.removeAttribute(physicsType);
+                setTimeout(function(){
+                    this.el.setAttribute(physicsType,this.originalPhysicsType);
+                }.bind(this),250);
+            }
+
+            this.el.setAttribute('position',this.data.respawnPosition);
+
+            this.el.emit('respawned');
         }
+    },
+
+    getPhysicsType: function(){
+        var physicsType = '';
+
+        if(this.el.hasAttribute('kinematic-body')){
+            physicsType = 'kinematic-body';
+        }else if(this.el.hasAttribute('dynamic-body')){
+            physicsType = 'dynamic-body';
+        }else{
+            physicsType = 'none';
+        }
+
+        return physicsType;
     }
 });
